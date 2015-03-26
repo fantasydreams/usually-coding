37c2c14a3912026ff909d263b90301dc8487b918
Carol <luoshengwen568@gmail.com>
2015-03-26 12:54:00 +0800

    N Hex convert to Decimal
    N Hex convert to Decimal

---
+++ ACM/N Hex convert to Decimal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+
+//integer Hex conversion function declare(m Hex convert to Decimal)
+long Hex(string num, int n);
+
+int main()
+{
+	string num;
+	int n;
+	cin >> num >> n;
+	cout << Hex(num, n);
+	return 0;
+}
+
+//function define and actualize
+
+long Hex(string num, int n)
+{
+	long result = 0;
+	for (int i = 0; i<num.size(); i++)
+	{
+		if ('0' <= num[i] <= '9')
+			result += (num[i] - '0') * pow(n, (num.size() - i - 1));
+		else if ('A' <= num[i] <= 'Z')
+			result += (num[i] - 'A' + 10) * pow(n, (num.size() - i - 1));
+		else if ('a' <= num[i] <= 'z')
+			result += (num[i] - 'a' + 10) * pow(n, (num.size() - i - 1));
+	}
+	return result;
+}
